79796c415d1d3c271081a5a7815099868235bac9
atik783505 <atik13672@gmail.com>
2026-06-20 02:41:58 +0600

    server start

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = process.env.MONGODB_URI;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+
+        const database = client.db('skillswap')
+        const taskscollection = database.collection('tasks')
+
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+
+        app.post('/api/tasks',async (req,res) => {
+            const task = req.body
+            const result = await taskscollection.insertOne(task)
+
+        })
+
+
+
+
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
