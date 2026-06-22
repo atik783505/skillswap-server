e379254a3cfd6a606252a8f96664cfd26514b56b
atik783505 <atik13672@gmail.com>
2026-06-22 11:45:18 +0600

    start Proposals api

--- index.js
+++ index.js
@@ -20,35 +20,63 @@ const client = new MongoClient(uri, {
 });
 async function run() {
     try {
-        
+
         await client.connect();
 
         const database = client.db('skillswap')
         const taskscollection = database.collection('tasks')
+        const proposalscollection = database.collection('proposals') 
 
         await client.db("admin").command({ ping: 1 });
 
-        app.post('/api/tasks',async (req,res) => {
+        app.post('/api/tasks', async (req, res) => {
             const task = req.body
             task.createdAt = new Date();
             const result = await taskscollection.insertOne(task)
             res.send(result)
         })
-        app.get('/api/tasks', async (req,res) => {
+        app.get('/api/tasks', async (req, res) => {
             const query = {}
-            if(req.query.clientId){
-                query.clientId=req.query.clientId
+            if (req.query.clientId) {
+                query.clientId = req.query.clientId
             }
             const result = await taskscollection.find(query).toArray()
             res.send(result)
         })
-        app.delete('/api/tasks/:id' , async (req,res) => {
-            const {id} = req.params
-            const query = {_id : new ObjectId(id)} 
+        app.get('/api/all-tasks', async (req,res) => {
+            const result = await taskscollection.find().toArray()
+            res.send(result)
+        })
+        app.get('/api/tasks/:id', async (req, res) => {
+            const { id } = req.params
+            const result = await taskscollection.findOne({ _id: new ObjectId(id) })
+            res.send(result)
+        })
+        app.delete('/api/tasks/:id', async (req, res) => {
+            const { id } = req.params
+            const query = { _id: new ObjectId(id) }
             const result = await taskscollection.deleteOne(query)
             res.send(result)
-        } )
+        })
 
+        app.patch('/api/tasks/:id', async (req, res) => {
+            try {
+                const { id } = req.params;
+                const updateData = req.body;
+                delete updateData._id;
+                delete updateData.status;
+
+                const result = await taskscollection.updateOne(
+                    { _id: new ObjectId(id) },
+                    { $set: updateData }
+                );
+
+                res.send(result);
+            } catch (error) {
+                console.error("Error updating task:", error);
+                res.status(500).send({ success: false, message: "Internal server error" });
+            }
+        });
 
 
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
